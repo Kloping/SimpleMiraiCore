d49793b0060a855ec886fae43bae5f470102e3de
HRS-Computer <3474006766@qq.com>
2021-10-15 13:14:59 +0800

    Core Console 一体

--- src/main/core/com/hrs/Kloping/Kotlin/Plugins/PluginLoader.kt
+++ src/main/core/com/hrs/Kloping/Kotlin/Plugins/PluginLoader.kt
@@ -5,7 +5,11 @@ import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader.main
 object PluginLoader {
     fun load(args: Array<String>) {
         try {
-            main(arrayOf())
+            val old_ps = System.out
+            val old_err = System.err
+            main(args)
+            System.setErr(old_err)
+            System.setOut(old_ps)
         } catch (e: Exception) {
             e.printStackTrace()
         }
