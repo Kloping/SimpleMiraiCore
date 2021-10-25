fea432e37876e1b0a76ff5bb21326d34d874b693
HRS-Computer <3474006766@qq.com>
2021-10-25 20:09:13 +0800

    增加更多消息类型的转义,避免报错

--- src/main/core/com/hrs/Kloping/Kotlin/utils/MessageUtils.kt
+++ src/main/core/com/hrs/Kloping/Kotlin/utils/MessageUtils.kt
@@ -1,16 +1,23 @@
 package com.hrs.Kloping.Kotlin.utils
 
 import net.mamoe.mirai.message.data.*
-import java.lang.StringBuilder
 
 object MessageUtils {
-    fun toText(messageChain: MessageChain): String {
+    /**
+     * 将消息链 转为 可匹配的 文本
+     * @param messageChain
+     * @return
+     */
+    fun toText(messageChain: MessageChain): String? {
         val sb = StringBuilder()
         for (message in messageChain) {
             if (message is MessageSource) continue
-            if (message is PlainText) sb.append(message.toString().trim { it <= ' ' })
-            if (message is At) sb.append("[@").append(message.target).append("]")
-            //.........
+            else if (message is PlainText) sb.append(message.toString().trim { it <= ' ' })
+            else if (message is At) sb.append("[@").append(message.target).append("]")
+            else if (message is Image) sb.append("[图片]")
+            else if (message is Audio) sb.append("[语音]")
+            else sb.append("[其他消息]")
+            //可以自己定义
         }
         return sb.toString()
     }
--- src/main/core/com/hrs/Kloping/java/utils/MessageUtils.java
+++ src/main/core/com/hrs/Kloping/java/utils/MessageUtils.java
@@ -3,13 +3,21 @@ package com.hrs.Kloping.java.utils;
 import net.mamoe.mirai.message.data.*;
 
 public class MessageUtils {
+    /**
+     * 将消息链 转为 可匹配的 文本
+     * @param messageChain
+     * @return
+     */
     public static String toText(MessageChain messageChain) {
         StringBuilder sb = new StringBuilder();
         for (Message message : messageChain) {
             if (message instanceof MessageSource) continue;
-            if (message instanceof PlainText) sb.append(message.toString().trim());
-            if (message instanceof At) sb.append("[@").append(((At) message).getTarget()).append("]");
-            //.........
+            else if (message instanceof PlainText) sb.append(message.toString().trim());
+            else if (message instanceof At) sb.append("[@").append(((At) message).getTarget()).append("]");
+            else if (message instanceof Image) sb.append("[图片]");
+            else if (message instanceof Audio) sb.append("[语音]");
+            else sb.append("[其他消息]");
+            //可以自己定义
         }
         return sb.toString();
     }
